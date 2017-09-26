76679d080131cc1b43fb42487e08f20e24358547
Carl Dalid <carlodalid@gmail.com>
2017-09-26 21:47:26 +0800

    Update model find functionality

--- api/models/objectModel.js
+++ api/models/objectModel.js
@@ -1,8 +1,8 @@
 var env = process.env.NODE_ENV || 'development';
-var db  = require('../../config/database')[env];
 var request = require('request');
 
-const dbURL = 'http://' + db.host + ':' + db.port + '/' + db.name;
+const db    = require('../../config/database')[env];
+const dbURL = "http://" + db.host + ":" + db.port + "/" + db.name;
 
 exports.save = function(jsonData, done) {
   request.post({
@@ -11,12 +11,35 @@ exports.save = function(jsonData, done) {
     json: true,
   }, function(error, response, body){
     if (error) return done('Unable to connect to CouchDB');
-    if (body.ok) {
+    if (response.statusCode === 200) {
       done(null, jsonData);
     }
   });
 }
 
-exports.find = function(key, timestamp) {
+exports.find = function(key, timestamp, done) {
+  var query = {
+    selector: {
+      key: key,
+      timestamp: { "$lte": timestamp }
+    },
+    sort: [{"timestamp":"desc"}],
+    limit: 1,
+    use_index: ["_design/8680bc258c23abf105f38477d00111ca605a8f08", "timestamp_index"]
+  }
 
+  request.post({
+    url: dbURL + "/_find",
+    body: query,
+    json: true
+  }, function(error, response, body){
+    if (error) return done('Unable to connect to CouchDB');
+
+    if (response.statusCode === 200) {
+      var obj = new Object;
+      obj.value = body.docs[0].value;
+
+      done(null, obj);
+    }
+  });
 }
